a2ec4d410e1820e6400e4410a8bb8a8f1744ed80
John Sullivan <jsullivan@csumb.edu>
2020-01-29 01:22:16 -0800

    Helix!

--- src/main/app/core.cljs
+++ src/main/app/core.cljs
@@ -1,16 +1,17 @@
 (ns app.core
   (:require [components.canvas :refer [CanvasTransform]]
-            [hx.react :as hx :refer [defnc]]
+            [helix.core :refer [defnc $]]
+            [helix.dom :as d]
             ["react-dom" :as react-dom]))
 
 (defnc ui
   [] 
-  [CanvasTransform {:transform (partial map (fn [[r g b a]] [g r b a]))
-                    :url "https://upload.wikimedia.org/wikipedia/commons/a/af/Tux.png"}])
+  ($ CanvasTransform {:transform (partial map (fn [[r g b a]] [g r b a]))
+                      :url "https://upload.wikimedia.org/wikipedia/commons/a/af/Tux.png"}))
 
 (defn render
   []
-  (react-dom/render (hx/f [ui])
+  (react-dom/render ($ ui)
                     (.getElementById js/document "app")))
 
 (defn init
--- src/main/components/canvas.cljs
+++ src/main/components/canvas.cljs
@@ -2,22 +2,24 @@
   "Canvas-related react components."
   (:require [util.core :refer [use-ref!]]
             [util.canvas :as cn]
-            [hx.react :as hx :refer [defnc]]
-            [hx.hooks :as hooks]))
+            [helix.core :refer [defnc $]]
+            [helix.hooks :as hooks]
+            [helix.dom :as d]))
 
 (defnc CanvasTransform
+  "Get image from `url`, apply `transform` to the RGBA pixels and display the result."
   [{:keys [transform url]}]
-  (let [!cref (hooks/useIRef nil)
-        [data set-data] (hooks/useState nil)]
+  (let [!cref (hooks/use-ref nil)
+        [data set-data] (hooks/use-state nil)]
 
-    (hooks/useEffect
-      #(cn/open-image! url set-data @!cref)
-      [url])
+    (hooks/use-effect
+      [url]
+      (cn/open-image! url set-data @!cref))
 
-    (hooks/useEffect
-      (fn []
-        (when data
-          (cn/transform-canvas! transform data @!cref)))
-      [transform data])
+    (hooks/use-effect
+      [transform data]
+      (when data
+        (cn/transform-canvas! transform data @!cref)))
 
-    [:div [:canvas {:ref (use-ref! cn/make-ref !cref)}]]))
+    (d/div
+      (d/canvas {:ref (use-ref! cn/make-ref !cref)}))))
