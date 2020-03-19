46d96ce5976c8c8efd010c025d58a4747b7fac71
John Sullivan <jsullivan@csumb.edu>
2020-03-19 01:05:02 -0700

    This is great!

--- src/stego/components/glcanvas.cljs
+++ src/stego/components/glcanvas.cljs
@@ -4,27 +4,34 @@
             [stego.canvas.drawing :as dr]
             [stego.gl.image :as im]
             [stego.components.util :as cm]
+            [play-cljc.gl.core :as glc]
             [helix.core :refer [defnc $]]
             [helix.hooks :as hooks]
             [helix.dom :as d]))
 
-(defn draw-callback
-  [{:keys [element context]} img]
-  (im/draw-image context {:image img}))
+(defn ->image [bmp]
+  (-> bmp
+      (dr/dimensions)
+      (assoc :data bmp)))
 
 (defnc GlCanvas
   [{:keys [img-source]}]
   (d/div
-    (let [img (cm/use-thunk cu/new-bitmap img-source)
+    (let [bmp (cm/use-thunk cu/new-bitmap img-source)
           canvas-ref (hooks/use-ref nil)]
-      (when img
-        (when-let [canvas @canvas-ref] (draw-callback canvas img))
-        (d/canvas
-          {:width 400
-           :height 400
-           :ref
-           #(when %
-              (dr/set-dimensions! % (dr/dimensions img))
-              (let [r {:element % :context (im/new-image-context %)}]
-                (reset! canvas-ref r)
-                (draw-callback r img)))})))))
+      (when bmp
+        (let [{:keys [width height] :as img} (->image bmp)]
+          (when-let [canvas @canvas-ref]
+            (let [image-bound (im/image-setup canvas img)]
+              (reset! canvas-ref image-bound)
+              (im/draw-image! image-bound)))
+          (d/canvas
+            {:width width
+             :height height
+             :ref
+             #(when %
+                (let [canvas (-> (.getContext % "webgl2")
+                                 (glc/->game)
+                                 (im/image-setup img))]
+                  (reset! canvas-ref canvas)
+                  (im/draw-image! canvas)))}))))))
--- src/stego/gl/image.cljs
+++ src/stego/gl/image.cljs
@@ -1,40 +1,29 @@
 (ns stego.gl.image
-  (:require [stego.gl.util :as gu]
-            [stego.canvas.drawing :as dr]
-            [iglu.core :refer [iglu->glsl]]))
+  (:require [play-cljc.gl.core :as glc]
+            [play-cljc.gl.entities-2d :as e2d]
+            [play-cljc.transforms :as transform]
+            [play-cljc.macros-js :refer-macros [gl]]))
 
-(defprotocol ImageContext
-  :extend-via-metadata true
-  (draw-image [context props] "Draw some provided image onto the gl canvas."))
+(defn image-setup
+  [game {:keys [data width height] :as image}]
+  (gl game disable (gl game CULL_FACE))
+  (gl game disable (gl game DEPTH_TEST))
+  (let [entity
+        (-> (e2d/->image-entity game data width height)
+            (assoc :clear {:color [1 1 1 1] :depth 1})
+            (assoc-in
+              [:fragment :functions 'main]
+              '([] (= o_color (.rrga (texture u_image v_tex_coord))))))]
+    (assoc game
+           :entity (glc/compile game entity)
+           :image image)))
 
-(def new-image-context
-  (gu/wrap-commands
-    (fn [rg]
-      {`draw-image
-       {:frag
-        (iglu->glsl
-          '{:precision "mediump float"
-            :varyings {uv vec2}
-            :uniforms {texture sampler2D}
-            :signatures {main ([] void)}
-            :functions
-            {main
-             ([] (= gl_FragColor (.brra (texture2D texture uv))))}})
-        :vert
-        (iglu->glsl
-          '{:precision "mediump float"
-            :varyings {uv vec2}
-            :attributes {position vec2}
-            :signatures {main ([] void)}
-            :functions
-            {main
-             ([] (= uv position)
-                 (= gl_Position (vec4 (- "1.0" (* "2.0" position)) 0 1)))}})
-        :attributes
-        {:position
-         [-2 0
-          0 -2
-          2 2]}
-        :uniforms
-        {:texture #(rg.texture (:image %2))}
-        :count 3}})))
+(defn draw-image!
+  [{:keys [image entity] :as game}]
+  (let [{:keys [width height]} image]
+    (glc/render
+      game (-> entity
+               (assoc :viewport {:x 0 :y 0 :width width :height height})
+               (transform/project width height)
+               (transform/translate 0 0)
+               (transform/scale width height)))))
